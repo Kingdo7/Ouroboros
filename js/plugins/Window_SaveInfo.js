61c4c35a9d3547e9777f45656930af498eadc557
Kingdo <j.bonnefond7@laposte.net>
2022-03-29 10:58:12 +0200

    [suppr] plugin inutiliser

--- js/plugins/Window_SaveInfo.js
+++
@@ -1,8 +0,0 @@
-﻿Window_SaveInfo.prototype.drawDarkRect = function(dx, dy, dw, dh) {
-//var color = this.gaugeBackColor(0); old
-//var color = '#5A3A22'; use this for hex color
-var color = this.textColor(0); //use this for MV color codes
-this.contents.paintOpacity = 0;
-this.contents.fillRect(dx + 1, dy + 1, dw - 2, dh - 2, color);
-this.changePaintOpacity(true);
-};
